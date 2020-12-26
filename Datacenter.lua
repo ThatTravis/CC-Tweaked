cae8cf9814a473b0d0efceef0d2150817237864f
thattravis <michaeljuengling@hotmail.de>
2020-12-26 17:50:21 +0100

    almost implemented excavate function

--- Datacenter.lua
+++ Datacenter.lua
@@ -1,25 +1,30 @@
 --script running on the data center computer
 
-modem = peripheral.wrap("top")
-monitor = peripheral.wrap("back")
+function wrapPeripherals()
+-- wraps modem and monitor in global variables
+    modem = peripheral.wrap("top")
+    monitor = peripheral.wrap("back")
+    
+    modem.open(0)
+    monitor.clear() 
+end
 
-modem.open(0)
-monitor.clear()
-
-while(true) do
-
-    event, modemSide, senderChannel, replyChannel, data, senderDistance = os.pullEvent("modem_message")
-
-    x, y, z, fuel = data
-
-
-    monitor.setCursorPos(1,1)
-    monitor.write("X: "..x)
-
-    monitor.setCursorPos(1,2)
-    monitor.write("Y: "..y)
-
-    monitor.setCursorPos(1,3)
-    monitor.write("Z: "..z)
-
+function receiveData()    
+    while(true) do
+    
+        event, modemSide, senderChannel, replyChannel, data, senderDistance = os.pullEvent("modem_message")
+    
+        x, y, z, fuel = data
+    
+    
+        monitor.setCursorPos(1,1)
+        monitor.write("X: "..x)
+    
+        monitor.setCursorPos(1,2)
+        monitor.write("Y: "..y)
+    
+        monitor.setCursorPos(1,3)
+        monitor.write("Z: "..z)
+    
+    end
 end
--- Turtle.lua
+++ Turtle.lua
@@ -38,25 +38,115 @@ function transmitData()
     modem.transmit(0,1,data)
 end
 
-function mine()
+function mine(mode)
 -- mining Script for Turtle
 -- saves starting location and takes x,y,z coordinates
 -- mines a cube from starting pos to x,y,z as furthest corner
 
-    modem = peripheral.wrap("left")
-
+    -- get position
     start = {}
-    start.x, start.x, start.z = gps.loacate()
+    start.x, start.y, start.z = gps.loacate()
 
-    destination = {}
-    print("Please enter your destination. X: ")
-    destination.x = read()
-    print("Please enter your destination. Y: ")
-    destination.y = read()
-    print("Please enter your destination. Z: ")
-    destination.z = read()
+    -- get direction turtle is facing (must be air)
+    local success, blockData = turtle.inspect()
+    if not success then
+        turtle.dig()
+    end
 
-    -- calc blocks to mine
-    totalBlocks = (destination.x-start.x+1)*(destination.y-start.y+1)*(destination.z-start.z+1)
+    turtle.forward()
+    pos.x, pos.y, pos.z = gps.locate()
+    if dir.x-start.x ==  1 then direction = "west" end  -- +X
+    if dir.x-start.x == -1 then direction = "east" end  -- -X
+    if dir.z-start.z ==  1 then direction = "south" end -- +Z
+    if dir.z-start.z == -1 then direction = "north" end -- -Z
+
+    do
+        mode = {
+        1 = mineExcavate()
+        2 = mineArea()
+    }
+    
+end
+
+function mineExcavate()
+-- Excavate mode for mine function
+
+    print("Please enter the size of the excavation area: ")
+    size = read()
+    print("1 = left, 2 = right ")
+    mode = read()
+    if mode == 1 then turn = "left" end
+    if mode == 2 then turn = "right" end
+    turtle.dig()
+    turtle.forward()
+    turtle.digUp()
+    turtle.digDown()
+
+    while pos.y > 3 do
+
+        for i = 1,size do -- mine a layer
+
+            for j = 1,size-1 do -- mine a strip
+                turtle.digUp()
+                turtle.dig()
+                turtle.digDown()
+                
+                -- if inventory full or fuel below 25% then dump and refuel
+                if turtle.getItemDetail(12) ~= nil or turtle.getFuelLevel/turtle.getFuelLimit < 0.25 then
+                    dropAndRefuel()
+                end
+
+                turtle.forward()
+            end
+            
+            if i < size then
+                -- turn the turtle for a new strip
+                if turn == "left" then 
+                    turtle.turnLeft()
+                    turtle.dig()
+                    turtle.digUp()
+                    turtle.digDown()
+                    turtle.forward()
+                    turtle.turnLeft()
+                    turtle.digUp()
+                    turtle.digDown()
+                end
+                if turn == "right" then 
+                    turtle.turnRight()
+                    turtle.dig()
+                    turtle.digUp()
+                    turtle.digDown()
+                    turtle.forward()
+                    turtle.turnRight()
+                    turtle.digUp()
+                    turtle.digDown()
+                end
+                switchTurn()
+            elseif i == size then
+
+            end
+            -- go back to start x,z
+        end
+
+            -- TODO!!! transition between layers
+            -- go down 2 if possible else 1
+
+    end
+
+
+
+
+
 
 end
+
+function mineArea()
+-- mine a specific area
+end
+
+function switchTurn()
+    if turn == "left" then turn = "right" end
+    if turn == "right" then turn = "left" end
+end
+
+
